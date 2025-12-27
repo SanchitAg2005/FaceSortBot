cbe5404d1e453af17693ce097a42c8b1e9010db4
Sanchit Agarwal <166907291+SanchitAg2005@users.noreply.github.com>
2025-12-28 00:36:10 +0530

    Change MongoDB connection string in db.py
    Updated MongoDB connection string to use a specific URI.

--- utils/db.py
+++ utils/db.py
@@ -1,6 +1,6 @@
 import os
 from pymongo import MongoClient
 
-client = MongoClient(os.environ["MONGO_URI"])
+client = MongoClient(os.environ["mongodb+srv://agarwalsanchit2005:SanchitDB2025_@facesortercluster.upc5259.mongodb.net/?appName=faceSorterCluster"])
 db = client["facesort"]
 users = db["telegram_users"]
