06fc7eeddf89e9aaae676cf338335941b34a353d
Sanchit Agarwal <166907291+SanchitAg2005@users.noreply.github.com>
2025-12-28 00:42:25 +0530

    Use environment variable for MongoDB connection
    Refactor MongoDB connection to use environment variable for URI.

--- utils/db.py
+++ utils/db.py
@@ -1,7 +1,13 @@
 import os
 from pymongo import MongoClient
 
-client = MongoClient(os.environ["mongodb+srv://agarwalsanchit2005:SanchitDB2025_@facesortercluster.upc5259.mongodb.net/?appName=faceSorterCluster"])
+MONGO_URI = os.environ.get("mongodb+srv://agarwalsanchit2005:SanchitDB2025_@facesortercluster.upc5259.mongodb.net/?appName=faceSorterCluster")
+
+if not MONGO_URI:
+    raise Exception("❌ MONGO_URI environment variable is missing!")
+
+client = MongoClient(MONGO_URI)
 db = client["facesort"]
 users = db["telegram_users"]
+
 
